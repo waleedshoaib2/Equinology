99651b8ebc1718f454932d891394c01bf74670d2
Waleed Shoaib <waleedshoaib20@gmail.com>
2025-03-02 22:20:27 +0500

    vercel fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000 // Optional: Set local dev server port
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
